fdb83475d5521a7d96b7e07582a951ed67dc7cef
Tory Stodard <tory.stodard@gmail.com>
2023-08-08 21:10:55 -0600

    added score logic to update score and UI.

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+let playerScore = 0;
+let computerScore = 0;
+
 const rockBtn = document.querySelector('#rock-btn');
 rockBtn.addEventListener('click', () => playRound('Rock'));
 
@@ -16,8 +19,10 @@ function playRound(playerSelection) {
     (playerSelection === 'Paper' && computerSelection === 'Rock') ||
     (playerSelection === 'Scissors' && computerSelection === 'Paper')
   ) {
+    updateScore('player');
     updateResults(playerSelection, computerSelection, 'You win this round!');
   } else {
+    updateScore('computer');
     updateResults(playerSelection, computerSelection, 'You loose this round!');
   }
 }
@@ -31,4 +36,19 @@ function getComputerSelection() {
 function updateResults(playerSelection, computerSelection, roundResult) {
   const resultsDiv = document.querySelector('#results');
   resultsDiv.textContent = `Player Chose: ${playerSelection} | Computer Chose: ${computerSelection} | ${roundResult}`;
+}
+
+function updateScore(winner) {
+  const playerScoreDiv = document.querySelector('#player-score');
+  const computerScoreDiv = document.querySelector('#computer-score');
+  switch (winner) {
+    case 'player':
+      playerScore++;
+      playerScoreDiv.textContent = `Player Score: ${playerScore}`;
+      break;
+    case 'computer':
+      computerScore++;
+      computerScoreDiv.textContent = `Computer Score: ${computerScore}`;
+      break;
+  }
 }
