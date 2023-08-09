3fed7dfbcc77e53180d0723300d0cfbe7604fde6
Tory Stodard <tory.stodard@gmail.com>
2023-08-08 20:58:29 -0600

    completed logic to play rounds and update results UI.

--- main.js
+++ main.js
@@ -8,11 +8,27 @@ const scissorsBtn = document.querySelector('#scissors-btn');
 scissorsBtn.addEventListener('click', () => playRound('Scissors'));
 
 function playRound(playerSelection) {
-  const computerSelection = getComuterSelection();
+  const computerSelection = getComputerSelection();
+  if (playerSelection === computerSelection) {
+    updateResults(playerSelection, computerSelection, 'Tie round!');
+  } else if (
+    (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+    (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+    (playerSelection === 'Scissors' && computerSelection === 'Paper')
+  ) {
+    updateResults(playerSelection, computerSelection, 'You win this round!');
+  } else {
+    updateResults(playerSelection, computerSelection, 'You loose this round!');
+  }
 }
 
 function getComputerSelection() {
-  const choices = ['rock', 'paper', 'scissors'];
+  const choices = ['Rock', 'Paper', 'Scissors'];
   const randChoice = Math.floor(Math.random() * choices.length);
   return choices[randChoice];
+}
+
+function updateResults(playerSelection, computerSelection, roundResult) {
+  const resultsDiv = document.querySelector('#results');
+  resultsDiv.textContent = `Player Chose: ${playerSelection} | Computer Chose: ${computerSelection} | ${roundResult}`;
 }
